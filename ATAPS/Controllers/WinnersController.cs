e494b9f53529ee4746a2835da6a4faae1fd3b300
Eric Adem <eric@aptuslogic.com>
2017-02-28 23:41:50 -0800

    Initial display screen code.  Making REST API calls and receiving JSON results.  Fetching a row from the database, but not incrementing row counter yet.

--- ATAPS/Controllers/WinnersController.cs
+++ ATAPS/Controllers/WinnersController.cs
@@ -1,4 +1,6 @@
-﻿using ATAPS.Models;
+﻿using System.Data;
+using System.Data.Entity;
+using ATAPS.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,8 @@ namespace ATAPS.Controllers
 {
     public class WinnersController : Controller
     {
+        private RFIDDBEntities db = new RFIDDBEntities();
+
         // GET: Winners
         public ActionResult Index()
         {
@@ -43,6 +47,53 @@ namespace ATAPS.Controllers
         public ActionResult Speaker()
         {
             return View();
+        }
+
+        // GET: Winners/Prev
+        public ActionResult Prev()
+        {
+            return View();//ega return json here
+        }
+        
+        // GET: Winners/Next
+        public ActionResult Next()
+        {
+            return View();//ega return json here
+        }
+
+        // GET: Winners/Reset
+        public ActionResult Reset()
+        {
+            return View();//ega return json here
+        }
+
+        // GET: Winners/DisplayUpdate
+        public ActionResult DisplayUpdate(int? prevID, int? filter)
+        {
+            //ega pass in an id param and do where clause
+
+            // grab a record from the database
+            Attendee attendee = db.Attendees.OrderBy (x => x.ID).FirstOrDefault();
+
+            // store it in a result array
+            IDictionary<string, string> data = new Dictionary<string, string>();
+            data["name"] = attendee.FirstName + " " + attendee.LastName;
+            data["id"] = attendee.ID.ToString ();
+
+            // json encode and return it
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Winners/PresenterUpdate
+        public ActionResult PresenterUpdate()
+        {
+            return View();//ega return json here
+        }
+
+        // GET: Winners/SpeakerUpdate
+        public ActionResult SpeakerUpdate()
+        {
+            return View();//ega return json here
         }
     }
 }
