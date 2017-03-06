7dee821366789c70a5edba2bd94c9229a207d9c7
JosephAdams <JosephAdams@DESKTOP-LT9HI4U>
2017-03-06 02:03:54 -0500

    Scaffold for Bus and Desk registration

---
+++ ATAPS/Controllers/RegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ATAPS.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET:  Registration/Busses
+        public ActionResult Busses()
+        {
+            return View();
+        }
+
+        public ActionResult BulkCheckin(string busNum)
+        {
+            ViewBag.BusNumber = busNum;
+            return View();
+        }
+
+        public ActionResult TableCheckin()
+        {
+            return View();
+        }
+    }
+}
