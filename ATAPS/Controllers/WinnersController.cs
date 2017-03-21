3340a0423dbb22aa69dea16d8a818e3c57ffd7a1
Eric Adem <eric@aptuslogic.com>
2017-03-21 13:27:53 -0700

    Admin screen has an "end parade" button and all three of the other winner's parade screens respond accordingly.

--- ATAPS/Controllers/WinnersController.cs
+++ ATAPS/Controllers/WinnersController.cs
@@ -101,6 +101,8 @@ namespace ATAPS.Controllers
             }
             else
             {
+                parm.ParmValue = "9999";
+                db.SaveChanges();
                 data["status"] = "failure";
             }
 
@@ -128,37 +130,50 @@ namespace ATAPS.Controllers
             // get current queue position
             Parm parm = db.Parms.Where(x => x.ParmName == "CurrWinnerQueuePos").First();
             int currQueuePos = int.Parse (parm.ParmValue);
-
-            // grab a record from the database
-            List<Attendee> attendees = new List<Attendee>();
-            attendees = db.Attendees.Where(x => x.WinnerQueueOrder >= currQueuePos).OrderBy(x => x.WinnerQueueOrder).ToList();
-
-            // store it in a result array
             IDictionary<string, string> data = new Dictionary<string, string>();
-            if (attendees.Count >= 1)
+            if (currQueuePos == 9999)
             {
-                data["curr_id"] = attendees[0].ID.ToString();
-                data["curr_name"] = attendees[0].FirstName + " " + attendees[0].LastName;
-                data["curr_phonetic"] = attendees[0].PhoneticFirst + " " + attendees[0].PhoneticLast;
-                data["curr_preferred"] = attendees[0].PreferredFirst + " " + attendees[0].PreferredLast;
-                data["curr_picture"] = attendees[0].Filename;
-                if (attendees.Count >= 2)
-                {
-                    data["next_id"] = attendees[1].ID.ToString();
-                    data["next_name"] = attendees[1].FirstName + " " + attendees[1].LastName;
-                    data["next_phonetic"] = attendees[1].PhoneticFirst + " " + attendees[1].PhoneticLast;
-                    data["next_preferred"] = attendees[1].PreferredFirst + " " + attendees[1].PreferredLast;
-                    data["next_picture"] = attendees[1].Filename;
-                }
+                data["can_click_prev"] = "false";
+                data["can_click_next"] = "false";
+                data["can_click_reset"] = "true";
+                data["status"] = "failure";
             }
+            else
+            {
+                // grab a record from the database
+                List<Attendee> attendees = new List<Attendee>();
+                attendees = db.Attendees.Where(x => x.WinnerQueueOrder >= currQueuePos).OrderBy(x => x.WinnerQueueOrder).ToList();
 
-            // see if are at the start or end of the list
-            attendees = db.Attendees.Where(x => x.WinnerQueueOrder != null).OrderBy(x => x.WinnerQueueOrder).ToList();
-            data["can_click_prev"] = (currQueuePos <= attendees[0].WinnerQueueOrder) ? "false" : "true";
-            data["can_click_next"] = (currQueuePos >= attendees[attendees.Count - 1].WinnerQueueOrder) ? "false" : "true";
+                // store it in a result array
+                if (attendees.Count >= 1)
+                {
+                    data["curr_id"] = attendees[0].ID.ToString();
+                    data["curr_name"] = attendees[0].FirstName + " " + attendees[0].LastName;
+                    data["curr_phonetic"] = attendees[0].PhoneticFirst + " " + attendees[0].PhoneticLast;
+                    data["curr_preferred"] = attendees[0].PreferredFirst + " " + attendees[0].PreferredLast;
+                    data["curr_picture"] = attendees[0].Filename;
+                    if (attendees.Count >= 2)
+                    {
+                        data["next_id"] = attendees[1].ID.ToString();
+                        data["next_name"] = attendees[1].FirstName + " " + attendees[1].LastName;
+                        data["next_phonetic"] = attendees[1].PhoneticFirst + " " + attendees[1].PhoneticLast;
+                        data["next_preferred"] = attendees[1].PreferredFirst + " " + attendees[1].PreferredLast;
+                        data["next_picture"] = attendees[1].Filename;
+                    }
+                }
 
-            // return current queue position
-            data["curr_queue_pos"] = currQueuePos.ToString();
+                // see if are at the start or end of the list
+                attendees = db.Attendees.Where(x => x.WinnerQueueOrder != null).OrderBy(x => x.WinnerQueueOrder).ToList();
+                data["can_click_reset"] = (currQueuePos <= attendees[0].WinnerQueueOrder) ? "false" : "true";
+                data["can_click_prev"] = (currQueuePos <= attendees[0].WinnerQueueOrder) ? "false" : "true";
+                data["can_click_next"] = (currQueuePos >= attendees[attendees.Count - 1].WinnerQueueOrder) ? "false" : "true";
+
+                // return current queue position
+                data["curr_queue_pos"] = currQueuePos.ToString();
+
+                // return success
+                data["status"] = "success";
+            }
 
             // json encode and return it
             return Json(data, JsonRequestBehavior.AllowGet);
