c8d492f3ae0f9c136b071dce28ece61bdce038d9
Eric Adem <eric@aptuslogic.com>
2017-03-26 23:13:01 -0700

    Admin screen now includes a "reset queue" link, and fixed a crash bug on display/presenter/speaker when no attendees are queued.

--- ATAPS/Controllers/WinnersController.cs
+++ ATAPS/Controllers/WinnersController.cs
@@ -192,48 +192,77 @@ namespace ATAPS.Controllers
                 // grab a record from the database
                 List<Attendee> attendees = new List<Attendee>();
                 attendees = db.Attendees.Where(x => x.WinnerQueueOrder >= currQueuePos).OrderBy(x => x.WinnerQueueOrder).ToList();
-                int? wqo = attendees[0].WinnerQueueOrder;
-
-                // store it in a result array
-                if (attendees.Count >= 1)
+                if (attendees.Count == 0)
                 {
-                    data["curr_id"] = attendees[0].ID.ToString();
-                    data["curr_name"] = attendees[0].FirstName + " " + attendees[0].LastName;
-                    data["curr_phonetic"] = attendees[0].PhoneticFirst + " " + attendees[0].PhoneticLast;
-                    data["curr_preferred"] = attendees[0].PreferredFirst + " " + attendees[0].PreferredLast;
-                    data["curr_picture"] = attendees[0].Filename;
-                    data["curr_title"] = attendees[0].Title;
-                    data["curr_dept"] = attendees[0].Department;
-                    if (attendees.Count >= 2)
-                    {
-                        data["next_id"] = attendees[1].ID.ToString();
-                        data["next_name"] = attendees[1].FirstName + " " + attendees[1].LastName;
-                        data["next_phonetic"] = attendees[1].PhoneticFirst + " " + attendees[1].PhoneticLast;
-                        data["next_preferred"] = attendees[1].PreferredFirst + " " + attendees[1].PreferredLast;
-                        data["next_picture"] = attendees[1].Filename;
-                        data["next_title"] = attendees[1].Title;
-                        data["next_dept"] = attendees[1].Department;
-                    }
+                    data["can_click_prev"] = "false";
+                    data["can_click_next"] = "false";
+                    data["can_click_reset"] = "true";
+                    data["status"] = "failure";
+                    data["parade_status"] = this.GetParadeStatus() ? "true" : "false";
                 }
+                else
+                {
+                    int? wqo = attendees[0].WinnerQueueOrder;
 
-                // see if are at the start or end of the list
-                attendees = db.Attendees.Where(x => x.WinnerQueueOrder != null).OrderBy(x => x.WinnerQueueOrder).ToList();
-                data["can_click_reset"] = (currQueuePos <= attendees[0].WinnerQueueOrder) ? "false" : "true";
-                data["can_click_prev"] = (currQueuePos <= attendees[0].WinnerQueueOrder) ? "false" : "true";
-                data["can_click_next"] = (currQueuePos >= attendees[attendees.Count - 1].WinnerQueueOrder) ? "false" : "true";
+                    // store it in a result array
+                    if (attendees.Count >= 1)
+                    {
+                        data["curr_id"] = attendees[0].ID.ToString();
+                        data["curr_name"] = attendees[0].FirstName + " " + attendees[0].LastName;
+                        data["curr_phonetic"] = attendees[0].PhoneticFirst + " " + attendees[0].PhoneticLast;
+                        data["curr_preferred"] = attendees[0].PreferredFirst + " " + attendees[0].PreferredLast;
+                        data["curr_picture"] = attendees[0].Filename;
+                        data["curr_title"] = attendees[0].Title;
+                        data["curr_dept"] = attendees[0].Department;
+                        if (attendees.Count >= 2)
+                        {
+                            data["next_id"] = attendees[1].ID.ToString();
+                            data["next_name"] = attendees[1].FirstName + " " + attendees[1].LastName;
+                            data["next_phonetic"] = attendees[1].PhoneticFirst + " " + attendees[1].PhoneticLast;
+                            data["next_preferred"] = attendees[1].PreferredFirst + " " + attendees[1].PreferredLast;
+                            data["next_picture"] = attendees[1].Filename;
+                            data["next_title"] = attendees[1].Title;
+                            data["next_dept"] = attendees[1].Department;
+                        }
+                    }
 
-                // return current queue position
-                data["curr_queue_pos"] = wqo.ToString();
+                    // see if are at the start or end of the list
+                    attendees = db.Attendees.Where(x => x.WinnerQueueOrder != null).OrderBy(x => x.WinnerQueueOrder).ToList();
+                    data["can_click_reset"] = (currQueuePos <= attendees[0].WinnerQueueOrder) ? "false" : "true";
+                    data["can_click_prev"] = (currQueuePos <= attendees[0].WinnerQueueOrder) ? "false" : "true";
+                    data["can_click_next"] = (currQueuePos >= attendees[attendees.Count - 1].WinnerQueueOrder) ? "false" : "true";
 
-                // return success
-                data["status"] = "success";
+                    // return current queue position
+                    data["curr_queue_pos"] = wqo.ToString();
 
-                // return parade status
-                data["parade_status"] = this.GetParadeStatus() ? "true" : "false";
+                    // return success
+                    data["status"] = "success";
+
+                    // return parade status
+                    data["parade_status"] = this.GetParadeStatus() ? "true" : "false";
+                }
             }
 
             // json encode and return it
             return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Winners/ResetOrder
+        public ActionResult ResetOrder()
+        {
+            // read all attendees and reset their queue order
+            List<Attendee> attendees = new List<Attendee>();
+            attendees = db.Attendees.ToList();
+            foreach (Attendee attendee in attendees)
+            {
+                attendee.WinnerQueueOrder = null;
+            }
+
+            // save changes
+            db.SaveChanges();
+
+            // redirect back to admin page
+            return RedirectToAction("Admin", new {});
         }
     }
 }
