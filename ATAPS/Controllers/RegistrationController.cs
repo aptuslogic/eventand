d805560543103c5dc67de560d9274ae839139717
Eric Adem <eric@aptuslogic.com>
2017-03-28 13:37:48 -0700

    Added ability to upload a variable number of waivers for each activity, and provide labels for them.

--- ATAPS/Controllers/RegistrationController.cs
+++ ATAPS/Controllers/RegistrationController.cs
@@ -38,10 +38,86 @@ namespace ATAPS.Controllers
             return View(regAgenda);
         }
 
-        public ActionResult BulkCheckin(string busNum)
+        public ActionResult BulkCheckin(string searchString, string busNum)
         {
             ViewBag.BusNumber = busNum;
-            return View();
+
+            int? pageNum = 1;
+            int pageSize = 25;
+            ViewBag.PageSize = pageSize;
+            // here we pull the query based on the sort order and direction
+            List<Attendee> attendees = new List<Attendee>();
+
+            string sortOrder = "name";
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.QueueSortParm = sortOrder == "WinnerQueueOrder" ? "wqo_desc" : "WinnerQueueOrder";
+            ViewBag.PartSortParm = sortOrder == "ParticipantType" ? "ptype_desc" : "ParticipantType";
+            ViewBag.RSVPSortParm = sortOrder == "RSVPStatus" ? "rsvp_desc" : "RSVPStatus";
+            ViewBag.RFIDSortParm = sortOrder == "RFID" ? "rfid_desc" : "RFID";
+            ViewBag.LastSort = sortOrder;
+            if (sortOrder == "") { ViewBag.LastSort = "Name"; }
+
+            // find max queue order and add one
+            List<Attendee> all_attendees = new List<Attendee>();
+            all_attendees = db.Attendees.OrderBy(x => x.WinnerQueueOrder).ToList();
+            int? last_queue_position = all_attendees[all_attendees.Count - 1].WinnerQueueOrder;
+            int next_queue_position = (last_queue_position == null) ? 1 : (int)(last_queue_position + 1);
+
+            // get attendees matching the search string
+            int eventId = int.Parse(ConfigurationManager.AppSettings["ActiveEvent"]);
+            attendees = ATAPS_Pile.GetSortedAttendeesWithFilter(eventId, sortOrder, searchString, pageNum);
+
+            if (pageNum == null)
+            {
+                pageNum = 1;
+            }
+            decimal pages = 1 + (attendees.Count() / pageSize);
+            if (pageNum * pageSize > attendees.Count())
+            {
+                int modCheck = attendees.Count() % pageSize; // get remainder
+                int lastPage = (int)Math.Ceiling(pages); // last page
+                attendees = attendees.Skip((lastPage - 1) * pageSize).Take(modCheck).ToList();
+            }
+            else
+            {
+                int startAt = (pageNum ?? default(int)) - 1;
+                attendees = attendees.Skip(startAt * pageSize).Take(pageSize).ToList();
+            }
+            // finally populate pagination tags
+
+            //decimal pages = 1 + (attendees.Count() / pageSize);
+            int upperLim = (int)pages;
+            int lowerLim = 1;
+            int curPage = pageNum ?? default(int);
+            ViewBag.CurPage = curPage;
+            ViewBag.Pages = upperLim;
+            ViewBag.LowerLim = lowerLim;
+            ViewBag.UpperLim = upperLim;
+
+            List<int> pageList = ATAPS_Pile.GetAttendeePageList(lowerLim, upperLim, curPage, attendees.Count());
+
+            ViewBag.PageList = pageList;
+
+            return View(attendees);
+        }
+
+        public ActionResult IndivCheckin(int attendeeId)
+        {
+            //ega pass in attendee id
+            return (View());
+        }
+
+        [HttpPost]
+        public ActionResult IndivCheckin()
+        {
+            //ega create a "checkin" as if attendee had just checked in to an activity
+            return (View());//ega send them back to bulk checkin
+        }
+
+        public ActionResult BulkRfid (int busID)
+        {
+            //ega populate list of attendees in order of check in
+            return (View());
         }
 
         public ActionResult TableCheckin()
